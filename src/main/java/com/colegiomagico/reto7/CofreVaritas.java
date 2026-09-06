f55a4552eb7f8a1679caaa5048e61f365776890a
juasebas202 <juansenita@gmail.com>
2026-09-06 17:41:13 +0000

    feat: Se implemento el arreglo para el reto 7

--- src/main/java/com/colegiomagico/reto7/CofreVaritas.java
+++ src/main/java/com/colegiomagico/reto7/CofreVaritas.java
@@ -6,8 +6,17 @@ package com.colegiomagico.reto7;
 public class CofreVaritas {
 
     public static int varitaMasPoderosa(int[] tamanos) {
-        return 0; // TODO: Cambia esto
+        int mayor = tamanos[0]; 
+
+        for (int tamano : tamanos) { 
+            if (tamano > mayor) {
+                mayor = tamano; 
+            }
+        }
+
+        return mayor;
     }
+    
 
     public static void main(String[] args) {
         int[] varitas = {10, 15, 8};
